005e5be2d4d0823ed2eec92c1e49c6724498c07a
Rittersm@gmail.com <rittersm@Seans-MacBook-Pro.local>
2016-07-06 16:52:43 -0400

    Admin page completed

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,9 @@
 class AdminController < ApplicationController
+
+  def verify
+    @title = "Welcome Administrator"
+    @content = "Thank you for your continued support of our page"
+    @promise = "We will have it up and running in no time"
+  end
+
 end

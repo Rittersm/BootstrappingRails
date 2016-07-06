8f8fb65dde8185d499ca0a496d3e5c421653d1e7
Rittersm@gmail.com <rittersm@Seans-MacBook-Pro.local>
2016-07-06 14:15:09 -0400

    Routes and login controller added

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,11 @@
 class LoginController < ApplicationController
+
+  def login
+    @title = "Welcome to my website"
+  end
+
+  def options
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'login#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'member/:name' => 'member#welcome' as :member
+    get 'admin/:admin_title' = 'admin#verify' as :admin
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

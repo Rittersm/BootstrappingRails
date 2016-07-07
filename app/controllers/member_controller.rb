ba2c4cb591651d7d2d4aa2f987fe6997fa48ae66
Rittersm@gmail.com <rittersm@Seans-MacBook-Pro.local>
2016-07-06 21:52:29 -0400

    Explorer mode completed

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,2 +1,9 @@
 class MemberController < ApplicationController
+
+  def welcome
+    @title = "Welcome Members"
+    @content = "Your support means a lot to us"
+    @promise = "With your support the site will be available soon"
+  end
+
 end
